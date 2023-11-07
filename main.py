5e502c07c84a541caaf8c442c832172074b513a0
Aleksey <alekseykalagin@gmail.com>
2023-11-07 21:25:15 +0300

    Test dog pk funcs

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from enum import Enum
 from typing import Union
 
 from fastapi import FastAPI, HTTPException
-from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 
 app = FastAPI()
